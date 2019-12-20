753c8b965e1639e0f66bf907fab25a5a7c812876
Nao Masuda <nmasuda@hanoverresearch.com>
2019-12-20 14:39:43 -0500

    test hello world

--- src/App.js
+++ src/App.js
@@ -1,67 +1,86 @@
 import React, { useState, useEffect } from "react"
-import { Spin } from "antd"
-import "./App.css"
-import Nav from "./components/Nav"
 
-/* global tableau */
+//Needed
 const { tableau } = window
 
 const App = () => {
-  const [loading, setLoading] = useState(true)
-  const [dashboard, setDashboard] = useState([])
-  const [profile, setProfile] = useState([])
-  const [height, setHeight] = useState()
+  const [dashboardName, setDashboardName] = useState("")
 
   useEffect(() => {
     tableau.extensions.initializeAsync().then(() => {
-      // tableau.extensions.settings.erase("dashboard")
-      // tableau.extensions.settings.erase("profile")
+      const dashboardName = tableau.extensions.dashboardContent.dashboard.name
 
-      // const containerHeight =
-      //   tableau.extensions.dashboardContent.dashboard.size.height
-
-      const dashboard = []
-      const profile = []
-
-      const promises = tableau.extensions.dashboardContent.dashboard.getParametersAsync()
-      // .then(d => {
-      //   return d.map(parameter => {
-      //     if (parameter.name === "Dashboard") {
-      //       return parameter.allowableValues.allowableValues.map(option => {
-      //         return dashboard.push(option.formattedValue)
-      //       })
-      //     } else if (parameter.name === "Profile") {
-      //       return parameter.allowableValues.allowableValues.map(option => {
-      //         return profile.push(option)
-      //       })
-      //     }
-      //   })
-      // })
-
-      // Wait for all requests, and then setData
-      promises.then(() => {
-        // tableau.extensions.settings.set("dashboard", dashboard)
-        // tableau.extensions.settings.set("profile", profile)
-        // console.log("dashboard", dashboard)
-        // console.log("profile", profile)
-        // setDashboard(dashboard)
-        // setProfile(profile)
-        setLoading(false)
-        // setHeight(containerHeight)
-      })
+      setDashboardName(dashboardName)
     })
-  }, [])
+  })
 
-  return (
-    <div>
-      {loading ? (
-        <Spin></Spin>
-      ) : (
-        <div>Test</div>
-        //<Nav dashboard={dashboard} profile={profile} height={height} />
-      )}
-    </div>
-  )
+  return <h1> Hello {dashboardName}</h1>
 }
+
+// import React, { useState, useEffect } from "react"
+// import { Spin } from "antd"
+// import "./App.css"
+// import Nav from "./components/Nav"
+
+// /* global tableau */
+// const { tableau } = window
+
+// const App = () => {
+//   const [loading, setLoading] = useState(true)
+//   const [dashboard, setDashboard] = useState([])
+//   const [profile, setProfile] = useState([])
+//   const [height, setHeight] = useState()
+
+//   useEffect(() => {
+//     tableau.extensions.initializeAsync().then(() => {
+//       // tableau.extensions.settings.erase("dashboard")
+//       // tableau.extensions.settings.erase("profile")
+
+//       // const containerHeight =
+//       //   tableau.extensions.dashboardContent.dashboard.size.height
+
+//       const dashboard = []
+//       const profile = []
+
+//       const promises = tableau.extensions.dashboardContent.dashboard.getParametersAsync()
+//       // .then(d => {
+//       //   return d.map(parameter => {
+//       //     if (parameter.name === "Dashboard") {
+//       //       return parameter.allowableValues.allowableValues.map(option => {
+//       //         return dashboard.push(option.formattedValue)
+//       //       })
+//       //     } else if (parameter.name === "Profile") {
+//       //       return parameter.allowableValues.allowableValues.map(option => {
+//       //         return profile.push(option)
+//       //       })
+//       //     }
+//       //   })
+//       // })
+
+//       // Wait for all requests, and then setData
+//       promises.then(() => {
+//         // tableau.extensions.settings.set("dashboard", dashboard)
+//         // tableau.extensions.settings.set("profile", profile)
+//         // console.log("dashboard", dashboard)
+//         // console.log("profile", profile)
+//         // setDashboard(dashboard)
+//         // setProfile(profile)
+//         setLoading(false)
+//         // setHeight(containerHeight)
+//       })
+//     })
+//   }, [])
+
+//   return (
+//     <div>
+//       {loading ? (
+//         <Spin></Spin>
+//       ) : (
+//         <div>Test</div>
+//         //<Nav dashboard={dashboard} profile={profile} height={height} />
+//       )}
+//     </div>
+//   )
+// }
 
 export default App
